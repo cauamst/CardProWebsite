dad17df63302c30865bb35a73a8dd09e675d8302
nguyenphuocdai <hoanghoang360@gmail.com>
2017-08-04 14:12:09 +0700

    Update handshake service

--- CardProWebsite/CardProWebsite/src/modules/app/services/handshake.service.ts
+++ CardProWebsite/CardProWebsite/src/modules/app/services/handshake.service.ts
@@ -2,53 +2,78 @@
 import { Http, Headers, RequestOptions, Response } from '@angular/http';
 import { HandShake } from '../models/HandShake';
 import { appConfig, HandShakeConfig } from '../../../app.config';
-import CryptoJS = require('crypto-js');
-import NodeRSA = require('node-rsa');
+import { CryptoUtils } from '../helpers/cryptoUtils';
 
 @Injectable()
 export class HandShakeService {
-    constructor(private http: Http) { }
+    constructor(private http: Http, private crypto: CryptoUtils) { }
 
     private doHandShake() {
-        var salt = CryptoJS.lib.WordArray.random(128 / 8);
-        //var key256Bits = CryptoJS.PBKDF2("Secret Passphrase", salt, { keySize: 256/32 });
-        let aesKey = CryptoJS.PBKDF2("Secret Passphrase", salt, { keySize: 256 / 32 }).toString();
-        let key = new NodeRSA(HandShakeConfig.publicKey);
-        var encryptedKey = key.encrypt(aesKey, 'base64');
-        console.log('encryptedKey: ', encryptedKey);
+        let pageKey: string;
+        let base64PageKey: string;
+        let encryptedPageKey: string;
+        let superKey: string;
+
+        let props = {
+            alg: 'A256GCM',
+            use: 'enc'
+        };
+        CryptoUtils
+            .generateKey("EC", 256, props)
+            .subscribe((aesKey) => {
+                var serializedKey = JSON.stringify(aesKey);
+                var base64Key : string = CryptoUtils.toBase64(serializedKey);
+
+                console.log("aesKey: " + aesKey);
+                console.log("serializedAesKey: " + serializedKey);
+                console.log("base64AesKey: " + base64Key);
+
+                pageKey = aesKey;
+                base64PageKey = base64Key;
+            });
+
+        CryptoUtils
+            .importKey(HandShakeConfig.publicKey)
+            .subscribe((rsaKey) => {
+                console.log("superKey: " + rsaKey);
+                superKey = rsaKey
+            });
+
+        CryptoUtils
+            .encrypt(base64PageKey, superKey)
+            .subscribe((encryptedContent) => {
+                console.log("jwe encrypted key:" + encryptedContent);
+                encryptedPageKey = encryptedContent;
+            });
 
         let handShakeDto = new HandShake();
-        handShakeDto.Key = encryptedKey;
+        handShakeDto.Key = encryptedPageKey;
 
         return this.http.post(HandShakeConfig.handshakeUrl, handShakeDto)
             .map((response: Response) => response.json())
             .map((dto: HandShake) =>
-                this.doChallenge(dto.Challenge, aesKey)
-                    ? { "key": dto.Key, "aesKey": aesKey }
-                    : { "key": dto.Key, "aesKey": aesKey });
+                this.doChallenge(dto.Challenge, pageKey, base64PageKey)
+                    ? { "key": dto.Key, "aesKey": pageKey }
+                    : null);
     }
 
-    private doChallenge(challenge, secret): boolean {
+    private doChallenge(challenge, secretKey, base64PageKey): boolean {
         console.log("challenge: " + challenge);
-        console.log("secret: " + secret);
-        let decrypted = CryptoJS.AES.decrypt(challenge, secret.toString());
-        console.log("doChallenge - decrypted: " + decrypted);
-        console.log("challenge is: " + decrypted == secret);
-        return decrypted == secret;
+        console.log("secret: " + secretKey);
+        let key = CryptoUtils.decryptFromKeyStore(challenge, secretKey);
+        let base64Key: string = CryptoUtils.toBase64(JSON.stringify(key));
+        console.log("base64PageKey : " + base64PageKey);
+        console.log("base64Key: " + base64Key);
+        return base64PageKey == base64Key;
     }
 
     handShake(data: object) {
         return this.doHandShake().map((obj) => {
             if (obj) {
-                var encryptedData = CryptoJS.AES.encrypt(JSON.stringify(data), obj.aesKey).ciphertext;
-                var wordArray = CryptoJS.enc.Utf8.parse(encryptedData);
-                var base64Data = CryptoJS.enc.Base64.stringify(wordArray);
-                console.log("base64Data: " + base64Data);
-                return { "ssId": obj.key, "data": base64Data };
+                var encryptedData = CryptoUtils.encrypt(JSON.stringify(data), obj.aesKey);
+                return { "ssId": obj.key, "data": encryptedData };
             }
             return null
-        }
-
-        );
+        });
     }
 }
