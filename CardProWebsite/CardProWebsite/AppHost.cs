a9341528800831828e6be7101dfa32c402d00208
51103220 <rewandedo1009@gmail.com>
2017-08-11 16:03:17 +0700

    Refactore app host

--- CardProWebsite/CardProWebsite/AppHost.cs
+++ CardProWebsite/CardProWebsite/AppHost.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Net;
-using System.Reflection;
-using System.Web;
+﻿using System.IO;
 using Funq;
 using CardProWebsite.ServiceInterface;
 using ServiceStack;
@@ -12,8 +6,7 @@ using ServiceStack.Configuration;
 
 namespace CardProWebsite {
     public class AppHost : AppHostBase {
-        /// Base constructor requires a Name and Assembly where web service implementation is located
-        /// </summary>
+
         public AppHost()
             : base("CardProWebsite", typeof(CardProService).Assembly) {
             var customSettings = new FileInfo(@"~/appsettings.txt".MapHostAbsolutePath());
@@ -22,19 +15,12 @@ namespace CardProWebsite {
                 : new AppSettings();
         }
 
-        /// <summary>
-        /// Application specific configuration
-        /// This method should initialize any Plugins or IoC dependencies used by your web services
-        /// </summary>
         public override void Configure(Container container) {
             SetConfig(new HostConfig {
                 DebugMode = AppSettings.Get("DebugMode", false),
                 AddRedirectParamsToQueryString = true
             });
 
-            //Other Plugin Examples
-            //Plugins.Add(new CorsFeature());
-            //Plugins.Add(new PostmanFeature());
         }
     }
 }
