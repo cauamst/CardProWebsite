6dc475bdce56a3aba282eeb00a189e4f3bba8b2c
nguyenphuocdai <hoanghoang360@gmail.com>
2017-08-17 10:32:04 +0700

    update layout home page send TTT

---
+++ CardProWebsite/CardProWebsite/src/shared/footer.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var tslib_1 = require("tslib");
+var core_1 = require("@angular/core");
+var FooterComponent = (function () {
+    function FooterComponent() {
+    }
+    return FooterComponent;
+}());
+FooterComponent = tslib_1.__decorate([
+    core_1.Component({
+        selector: 'app-footer',
+        template: "\n        <footer class=\"mdl-mega-footer\">\n          <div class=\"mdl-mega-footer--middle-section\">\n            <div class=\"mdl-mega-footer--drop-down-section\">\n              <input class=\"mdl-mega-footer--heading-checkbox\" type=\"checkbox\" checked>\n              <h1 class=\"mdl-mega-footer--heading\">Features</h1>\n              <ul class=\"mdl-mega-footer--link-list\">\n                <li><a href=\"#\">About</a></li>\n                <li><a href=\"#\">Terms</a></li>\n                <li><a href=\"#\">Partners</a></li>\n                <li><a href=\"#\">Updates</a></li>\n              </ul>\n            </div>\n            <div class=\"mdl-mega-footer--drop-down-section\">\n              <input class=\"mdl-mega-footer--heading-checkbox\" type=\"checkbox\" checked>\n              <h1 class=\"mdl-mega-footer--heading\">Details</h1>\n              <ul class=\"mdl-mega-footer--link-list\">\n                <li><a href=\"#\">Spec</a></li>\n                <li><a href=\"#\">Tools</a></li>\n                <li><a href=\"#\">Resources</a></li>\n              </ul>\n            </div>\n            <div class=\"mdl-mega-footer--drop-down-section\">\n              <input class=\"mdl-mega-footer--heading-checkbox\" type=\"checkbox\" checked>\n              <h1 class=\"mdl-mega-footer--heading\">Technology</h1>\n              <ul class=\"mdl-mega-footer--link-list\">\n                <li><a href=\"#\">How it works</a></li>\n                <li><a href=\"#\">Patterns</a></li>\n                <li><a href=\"#\">Usage</a></li>\n                <li><a href=\"#\">Products</a></li>\n                <li><a href=\"#\">Contracts</a></li>\n              </ul>\n            </div>\n            <div class=\"mdl-mega-footer--drop-down-section\">\n              <input class=\"mdl-mega-footer--heading-checkbox\" type=\"checkbox\" checked>\n              <h1 class=\"mdl-mega-footer--heading\">FAQ</h1>\n              <ul class=\"mdl-mega-footer--link-list\">\n                <li><a href=\"#\">Questions</a></li>\n                <li><a href=\"#\">Answers</a></li>\n                <li><a href=\"#\">Contact us</a></li>\n              </ul>\n            </div>\n          </div>\n          <div class=\"mdl-mega-footer--bottom-section\">\n            <div class=\"mdl-logo\">\n              More Information\n            </div>\n            <ul class=\"mdl-mega-footer--link-list\">\n              <li>266-268, Nam K\u1EF3 Kh\u1EDFi Ngh\u0129a, Ph\u01B0\u1EDDng 8, Qu\u1EADn 3, H\u1ED3 Ch\u00ED Minh: Ng\u00E2n h\u00E0ng Sacombank, Trung t\u00E2m th\u1EBB Sacombank ... 266 - 268 , Nam K\u1EF3 Kh\u1EDFi Ngh\u0129a , Qu\u1EADn 3, H\u1ED3 Ch\u00ED Minh</li>\n            </ul>\n          </div>\n        </footer>\n    ",
+        styles: ["\n        .card_pro_footer {\n            margin-top: 80px;\n         }\n\n    "]
+    })
+], FooterComponent);
+exports.FooterComponent = FooterComponent;
+//# sourceMappingURL=footer.js.map
