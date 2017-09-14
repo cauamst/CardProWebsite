f0aaaf2437d50217f05ed6369ab76273de2816a9
51103220 <rewandedo1009@gmail.com>
2017-09-14 13:25:53 +0700

    Fix select box bugs in compare page

--- CardProWebsite/CardProWebsite/src/modules/app/register-credit/compare.ts
+++ CardProWebsite/CardProWebsite/src/modules/app/register-credit/compare.ts
@@ -14,33 +14,28 @@ import { NotificationService } from '../../../shared/utils';
 
 })
 export class CompareComponent implements OnInit {
-    currentCatId1: number
-    currentCatId2: number;
-    selectCardId1: number;
-    selectCardId2: number;
-    showDialog1 = false;
-    showDialog2 = false;
-    cards: CARD[] = [];
+    showDialog1: boolean;
+    showDialog2: boolean;
+    cat1Id: number;
+    cat2Id: number;
     card1: CARD;
     card2: CARD;
-    cardes1: CARD[];
-    cardes2: CARD[];
-    ShowimageCompare1 = true;
-    ShowimageCompare2 = true;
-
-    isAddClass: boolean = false;
-    isAddClass1: boolean = false;
-    isAddClass2: boolean = false;
-    textPickCard1: string = "Chọn thẻ này";
-    textPickCard2: string = "Chọn thẻ này";
-
+    cards1: CARD[];
+    cards2: CARD[];
+    isAddClass1: boolean;
+    isAddClass2: boolean;
     initialWidth: number;
-
-    selectedCard1ToForm: CARD;
-    selectedCard2ToForm: CARD;
+    tempCard: CARD;
 
     @Output('AddCardToFormEvent')
     selectedCardToFormEvent = new EventEmitter();
+
+    readonly benefits = [
+        { currentCatId: 1, director: 'Dặm bay' },
+        { currentCatId: 2, director: 'Hoàn tiền' },
+        { currentCatId: 3, director: 'Điểm thưởng' },
+        { currentCatId: 4, director: 'Rút tiền mặt miễn phí' },
+    ]
 
     constructor(
         private location: Location,
@@ -52,83 +47,67 @@ export class CompareComponent implements OnInit {
 
 
     ngOnInit(): void {
-        this.GetCards();
-        this.getCard1(this.selectCardId1);
-        this.getCard2(this.selectCardId2);
-        this.getCardType1(this.currentCatId1);
-        this.getCardType2(this.currentCatId2);
-        this.ShowimageCompare1;
-        this.ShowimageCompare2;
     }
-    benefit1 = [
-        { currentCatId: 1, director: 'Dặm bay' },
-        { currentCatId: 2, director: 'Hoàn tiền' },
-        { currentCatId: 3, director: 'Điểm thưởng' },
-        { currentCatId: 4, director: 'Rút tiền mặt miễn phí' },
-    ];
-    benefit2 = [
-        { currentCatId: 1, director: 'Dặm bay' },
-        { currentCatId: 2, director: 'Hoàn tiền' },
-        { currentCatId: 3, director: 'Điểm thưởng' },
-        { currentCatId: 4, director: 'Rút tiền mặt miễn phí' },
-    ];
 
-    //get card by id for creditdetail page
-    getCard2(Id: number): void {
-        this.CardService.getCard(Id).then(card => {
-            this.card2 = card;
-        });
+    selectTempCard(card: CARD) {
+        this.tempCard = card;
     }
-    getCard1(Id: number): void {
-        this.CardService.getCard(Id).then(card => {
+
+    getCard(card: CARD, firstCard = false): void {
+        if (firstCard) {
             this.card1 = card;
-        });
+        } else {
+            this.card2 = card;
+        }
     }
-    //get all card table compare
-    GetCards(): void {
-        this.CardService.getAllCard().then(cards => {
-            this.cards = cards;
-        });
+
+    getCardByType(typeId: number, firstCard = false): void {
+        if (typeId !== -1) {
+            this.CardService.getCardType(typeId).then(cards => {
+                if (firstCard) {
+                    this.cards1 = cards;
+                }
+                else {
+                    this.cards2 = cards;
+                }
+            });
+        } else {
+            if (firstCard) {
+                this.cards1 = null;
+
+            } else {
+                this.cards2 = null;
+            }
+        }
+        this.tempCard = null;
     }
-    getCardType1(type: number): void {
-        this.CardService.getCardType(type).then(cardes => {
-            this.cardes1 = cardes;
-        });
-    }
-    getCardType2(type: number): void {
-        this.CardService.getCardType(type).then(cardes => {
-            this.cardes2 = cardes;
-        });
-    }
-    RemoveCard1(): void {
-        this.card1 = null;
-        this.card1 = this.card2;
-        this.card2 = null;
-    }
-    RemoveCard2(): void {
-        this.card2 = null;
-    }
-    UnSelectedCard1(): void
-    {
-        this.currentCatId1 = undefined;
-        this.selectCardId1 = undefined;
-    }
-    UnSelectedCard2(): void {
-        this.currentCatId2 = undefined;
-        this.selectCardId2 = undefined;
-    }
-    onSelectedCard1(): void {
-        this.selectedCard1ToForm = this.card1;
-        console.log(this.selectedCard1ToForm);
-    }
-    onSelectedCard2(): void {
-        this.selectedCard2ToForm = this.card2;
-        console.log(this.selectedCard2ToForm);
+
+    removeCard(firstCard = false): void {
+        if (firstCard) {
+            this.card1 = this.card2;
+            this.card2 = null;
+        } else {
+            this.card2 = null;
+        }
     }
 
     selectCard(card: CARD) {
         this.selectedCardToFormEvent.emit(card);
     }
+
+    confirm(isFirstCard: boolean, isOk = false) {
+        if (isFirstCard) {
+            this.card1 = isOk ? this.tempCard : null;
+        } else {
+            this.card2 = isOk ? this.tempCard : null;
+        }
+        this.cat1Id = undefined;
+        this.cat2Id = undefined;
+        this.tempCard = null;
+        this.cards1 = null;
+        this.cards2 = null;
+    }
+
     moveToTopButton() {
         let width = window.innerWidth;
         this.initialWidth = width;
@@ -136,6 +115,7 @@ export class CompareComponent implements OnInit {
             this.NotificationService.needToTop(true);
         }
     }
+
     addClassbutton(n: number) {
         switch (n) {
             case 1:
@@ -161,6 +141,6 @@ export class CompareComponent implements OnInit {
         } else {
             this.selectCard(null);
         }
-       
+
     }
 }
