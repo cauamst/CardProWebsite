f99b29bc549a71ee4502b6fae08d0fbf4909cbb2
nguyenphuocdai <hoanghoang360@gmail.com>
2017-09-18 17:23:47 +0700

    changes preloading, fix footer

--- CardProWebsite/CardProWebsite/src/shared/footer.ts
+++ CardProWebsite/CardProWebsite/src/shared/footer.ts
@@ -6,17 +6,27 @@
     styles: [`
 
 .headline{
-    width: 100%;
+    width: 85%;
     border: 0 !important;
     display: block;
     margin: -10px 0;
     box-sizing: border-box;
 }
+ul {
+    padding-left: 0 !important;
+    list-style: none;
+    margin-bottom: 10px;
+}
+.sacombank-width-li{
+    width: 85%;
+}
+li{
+    border-bottom : solid 1px #353535;
+}
 address{
     line-height: 23px;
     color: #eee;
-    margin-top: 20px;
-    margin-bottom: 20px;
+    margin-bottom: 15px;
     font-style: normal;
 }
 h2 {
@@ -26,7 +36,31 @@ h2 {
     font-size: 1.4em !important;
     padding-bottom: 0 !important;
 }
-    
+a{
+    color : #fff !important;
+}
+.position-arrow {
+    position: absolute !important;
+}
+li i {
+    float : right;
+}
+.image-li{
+    height : 40px;
+    width : 80px;
+    vertical-align: middle;
+    border: 0;
+}
+.none-border {
+    border-bottom : 0 !important;
+}
+.span-footer-address {
+    margin-left: 30px !important;
+}   
+.sacombank-footer{
+    text-align: center !important;
+    background-color :#3e4753 !important;
+}
 `]
 })
 export class FooterComponent { }
